aabeebed5077da37f7b99d5ccca8619d2810ebb9
labordean <labardium@gmail.com>
2026-07-09 16:58:04 +0300

    Perf: переход на загрузку изображений из Cloudinary вместо загрузки с сервера

--- app.py
+++ app.py
@@ -17,10 +17,11 @@ app.secret_key = os.environ.get('SECRET_KEY', 'default_fallback_key_for_dev')
 json_path = os.path.join(basedir, 'quotes.json')
 quotes_list = json.loads(open(json_path, encoding='utf-8').read())['quotes']
 
-image_path = os.path.join(basedir, 'static', 'images', 'background')
-bg_list = os.listdir(image_path)
-
 app.permanent_session_lifetime = timedelta(days=30)
+
+cloud_name = "hligceqf"
+
+images = ['monet1', 'monet2', 'monet3', 'monet4', 'monet5', 'monet6', 'monet7', 'monet8', 'monet9', 'monet10', 'monet11', 'monet12', 'monet13']
 
 @app.route("/")
 def print_quote():
@@ -32,7 +33,7 @@ def print_quote():
 
     # 2. Высчитываем лимит один раз и используем безопасное сравнение
     max_history_length = int(len(quotes_list) * 0.5)
-    max_background_length = int(len(bg_list) * 0.5)
+    max_background_length = int(len(images) * 0.5)
 
     # Если базу урезали, и история стала больше базы, обрезаем ее с запасом
     while len(session['history']) >= max_history_length:
@@ -52,9 +53,9 @@ def print_quote():
     if 'background' not in session:
         session['background'] = []
     if not session['background']:  # пустой список
-        random_background = choice(bg_list)
+        current_image = choice(images)
     else:
-        random_background = session['background'][-1]
+        current_image = session['background'][-1]
 
     if 'counter_list' not in session:
         session['counter_list'] = []
@@ -66,16 +67,24 @@ def print_quote():
 
     if len(session['counter_list']) % 5 == 0:
         session['counter_list'] = []
-        while random_background in session['background']:
-            random_background = choice(bg_list)
-
+        while current_image in session['background']:
+            current_image = choice(images)
 
     if session['background']:
-        if session['background'][-1] != random_background:
-            session['background'].append(random_background)
+        if session['background'][-1] != current_image:
+            session['background'].append(current_image)
     else:
-        session['background'].append(random_background)
+        session['background'].append(current_image)
+
+    def cloudinary_url(image, width):
+        return f"https://res.cloudinary.com/{cloud_name}/image/upload/w_{width},f_avif,q_auto:good/{image}"
+
+    bg_urls = {
+        "768": cloudinary_url(current_image, 768),
+        "1280": cloudinary_url(current_image, 1280),
+        "1920": cloudinary_url(current_image, 1920),
+        "2560": cloudinary_url(current_image, 2560)
+    }
 
     session.modified = True
-    random_bg_path = 'images/background/' + random_background
-    return render_template('index.html', quote=random_quote, bg_path=random_bg_path)
+    return render_template('index.html', quote=random_quote, bg_urls=bg_urls)
